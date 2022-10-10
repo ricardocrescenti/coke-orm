75d874843ac885c716eaabcf2a998a19b5dbd6db
Ricardo Crescenti <ricardo.crescenti@gmail.com>
2022-10-10 09:36:13 -0300

    Versão 0.17.1
    Alteração tipo da propriedade `target` do `SubscriberOptions` para aceitar uma `Function` ou `null`, por causa dos Subscribers gerais, que podem ser usados para todos os models.

--- src/decorators/event/event-subscriber.ts
+++ src/decorators/event/event-subscriber.ts
@@ -1,13 +1,20 @@
-import { ConstructorTo } from "../../common";
-import { EntitySubscriberInterface } from "../../metadata";
-import { CokeModel } from "../../manager";
-import { DecoratorsStore } from "../decorators-store";
+import { ConstructorTo } from '../../common';
+import { EntitySubscriberInterface } from '../../metadata';
+import { CokeModel } from '../../manager';
+import { DecoratorsStore } from '../decorators-store';
 
-export function EventsSubscriber(entity: ConstructorTo<CokeModel>): ClassDecorator {
-   return function (target: Function) {
-      DecoratorsStore.addSubscriber({
-         target: entity,
-         subscriber: target as ConstructorTo<EntitySubscriberInterface<any>>
-      });
-   };
+/**
+ * Decorator for subscriber implementation
+ * @param {ConstructorTo} entity Model creation function
+ * @return {ClassDecorator} Function used to implement decorators
+ */
+export function EventsSubscriber(entity: ConstructorTo<CokeModel> | null): ClassDecorator {
+
+	return function(target: Function) {
+		DecoratorsStore.addSubscriber({
+			target: entity,
+			subscriber: target as ConstructorTo<EntitySubscriberInterface<any>>,
+		});
+	};
+
 }
